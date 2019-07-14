4e92034bbae92981a9a0721dc6fdcf68659a03e4
mhouston4994 <mhouston4994@gmail.com>
2019-07-13 19:46:58 -0600

    updated css, navbar, jumbotron, menu button js

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,7 @@
 // Select DOM Items
-const menuBtn = document.querySelector(".menu-btn");
+const menuBtn = $(".menu-btn");
+const bootstrapMenuBtn = $(".navbar-toggler");
 
-// Set Initial State Of Menu
-var showMenu = false;
-menuBtn.addEventListener("click", toggleMenuAnimation);
-
-function toggleMenuAnimation() {
-    if(!showMenu) {
-        menuBtn.classList.add("close");
-        // Set Menu State
-        showMenu = true;
-    } else {
-        menuBtn.classList.remove("close");
-        //Set Menu State
-        showMenu = false;
-    }
-}
+bootstrapMenuBtn.click(function(){
+    menuBtn.toggleClass("close");
+});
